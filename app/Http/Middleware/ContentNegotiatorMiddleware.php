447d0168b3142a3a67e7c0f4f6e412b45977db4b
Babibubebon <babibubebon@babibubebo.org>
2020-01-20 01:09:35 +0900

    fix incorrct content-type header

--- app/Http/Middleware/ContentNegotiatorMiddleware.php
+++ app/Http/Middleware/ContentNegotiatorMiddleware.php
@@ -9,22 +9,33 @@ class ContentNegotiatorMiddleware
 {
     public $defaultType = 'text/html';
 
-    public $acceptableTypes = [
+    public static $acceptableTypes = [
         'text/html' => 'html',
         'application/xhtml+xml' => 'html',
         'text/n3' => 'data',
         'text/turtle' => 'data',
         'application/n-triples' => 'data',
         'application/rdf+xml' => 'data',
+        'application/json' => 'data',
+        'application/ld+json' => 'data',
     ];
 
-    public $fileExtensions = [
+    public static $fileExtensions = [
         'text/html' => '.html',
         'application/xhtml+xml' => '.html',
         'text/turtle' => '.ttl',
         'application/n-triples' => '.nt',
         'application/rdf+xml' => '.rdf',
+        'application/json' => '.json',
+        'application/ld+json' => '.jsonld',
     ];
+
+    public static function mimetypeFromExtension($ext)
+    {
+        $extensionToMimetype = array_flip(self::$fileExtensions);
+        return array_key_exists($ext, $extensionToMimetype)
+            ? $extensionToMimetype[$ext] : null;
+    }
 
     /**
      * Handle an incoming request.
@@ -53,15 +64,15 @@ class ContentNegotiatorMiddleware
         $id = $request->route('id');
         $negotiatedType = $this->defaultType;
         foreach ($accepts as $mime => $q) {
-            if (array_key_exists($mime, $this->acceptableTypes)) {
+            if (array_key_exists($mime, self::$acceptableTypes)) {
                 $negotiatedType = $mime;
             }
         }
 
-        $redirectTo = $this->acceptableTypes[$negotiatedType] . '.' . $datasetName;
+        $redirectTo = self::$acceptableTypes[$negotiatedType] . '.' . $datasetName;
         $params = ['id' => $id];
         if (substr($redirectTo, 0, 4) === 'data') {
-            $params['ext'] = $this->fileExtensions[$negotiatedType];
+            $params['ext'] = self::$fileExtensions[$negotiatedType];
         }
         return redirect()->route($redirectTo, $params, 303);
     }
