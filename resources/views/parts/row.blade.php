e581e6adf297035d08836bf16c0b54d1b655b672
Babibubebon <babibubebon@babibubebo.org>
2020-08-27 01:00:58 +0900

    Update EasyRdf to v1.0.0

--- resources/views/parts/row.blade.php
+++ resources/views/parts/row.blade.php
@@ -3,7 +3,7 @@
         @if($subject !== $primaryTopic)
             is
         @endif
-        <a href="{{ $predicate }}">{{ \EasyRdf_Namespace::shorten($predicate) ?? $predicate }}</a>
+        <a href="{{ $predicate }}">{{ \EasyRdf\RdfNamespace::shorten($predicate) ?? $predicate }}</a>
         @if($subject !== $primaryTopic)
             of
         @endif
@@ -14,14 +14,14 @@
                 @foreach($objects as $object)
                     <li>
                         @if($object['type'] === 'uri')
-                            <a href="{{ $object['value'] }}">{{ \EasyRdf_Namespace::shorten($object['value']) ?? $object['value'] }}</a>
+                            <a href="{{ $object['value'] }}">{{ \EasyRdf\RdfNamespace::shorten($object['value']) ?? $object['value'] }}</a>
                         @elseif($object['type'] === 'literal')
                             {{ $object['value'] }}
                             @if(isset($object['lang']))
                                 <small class="langtag">{{ '@'.$object['lang'] }}</small>
                             @endif
                             @if(isset($object['datatype']))
-                                <small class="datatype">^^{{ \EasyRdf_Namespace::shorten($object['datatype']) ?? $object['datatype'] }}</small>
+                                <small class="datatype">^^{{ \EasyRdf\RdfNamespace::shorten($object['datatype']) ?? $object['datatype'] }}</small>
                             @endif
                         @endif
                     </li>
